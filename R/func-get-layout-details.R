9611047a5580a413e480318f3d4cbf8c3539ee44
AyushBipinPatel <ayush.ap58@gmail.com>
2023-08-07 11:07:20 +0530

    function def to get layout details of txt files from the xlsx layout file.

--- R/func-get-layout-details.R
+++ R/func-get-layout-details.R
@@ -7,20 +7,23 @@
 #' expected by readxl::read_xlsx 
 #' @param pass_range Range of the table in the layout of the xlsx
 #' file as expected yb readxl::read_xlsx
+#' @param obj_name Name for the layout data frame
 #'
-#' @return
+#' @return a layout dataframe
 #' @export
 #'
 #' @examples
 
 
-get_layout_details <- function(pass_sheet,pass_range){
+get_layout_details <- function(pass_sheet,pass_range,obj_name){
   
   readxl::read_xlsx(
     path = here("data-plfs-2021-2022/data layout/4Data_LayoutPLFS_2021-22.xlsx"),
     sheet = pass_sheet,
     range = pass_range
   )|>
-    janitor::clean_names()
+    janitor::clean_names() -> temp
+  
+  return(assign(x = obj_name,value = temp,envir = .GlobalEnv))
   
 }
